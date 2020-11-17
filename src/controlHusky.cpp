d585309931b19b000941c4ec3aadc38ab1b14799
Yogesh Patil <66321716+yogeshp-1411@users.noreply.github.com>
2020-11-17 19:24:10 +0100

    Add files via upload
    Resolved minor bugs. (Updated Code)

--- src/controlHusky.cpp
+++ src/controlHusky.cpp
@@ -5,95 +5,143 @@
 #include "tf/tf.h"
 #include "angles/angles.h"
 
-float current_x, current_y;
+float current_x = 1; 
+float current_y = 1;
 double theta_degree;
-float goal_x = 5;
-float goal_y = 3;
+bool clock_wise = false;
+float goal_x = 0;
+float goal_y = 0;
 
 void callBack(const nav_msgs::Odometry::ConstPtr& msg)
 {
-	current_x = msg->pose.pose.position.x;
-	current_y = msg->pose.pose.position.y;
-	
-	//ROS_INFO("Position-> x: [%f], y: [%f], z: [%f]", msg->pose.pose.position.x, msg->pose.pose.position.y, msg->pose.pose.position.z);
-	//ROS_INFO("Orientation-> x: [%f], y: [%f], z: [%f], w: [%f]", msg->pose.pose.orientation.x, msg->pose.pose.orientation.y, msg->pose.pose.orientation.z, msg->pose.pose.orientation.w);
-	//ROS_INFO("Vel-> Linear: [%f], Angular: [%f]", msg->twist.twist.linear.x,msg->twist.twist.angular.z);
-
-	/*float orientation[3];
-	orientation[0] = msg->pose.pose.orientation.x;
-	orientation[1] = msg->pose.pose.orientation.y;
-	orientation[2] = msg->pose.pose.orientation.z;
-	orientation[3] = msg->pose.pose.orientation.w;*/
-	//printf("Orientation-> x: [%f], y: [%f], z: [%f], w: [%f] \n", orientation[0], orientation[1], orientation[2], orientation[3]);
+	current_x = (msg->pose.pose.position.x);
+	current_y = (msg->pose.pose.position.y);
 
 	tf::Quaternion q( msg->pose.pose.orientation.x,
 		 	msg->pose.pose.orientation.y,
 			msg->pose.pose.orientation.z,
 			msg->pose.pose.orientation.w);
 
-	//orientation.getRPY(roll, pitch, yaw);
-
 	double theta_yaw = tf::getYaw(q);
-	theta_degree = angles::to_degrees(angles::normalize_angle_positive(theta_yaw));
-//	theta_degree += 90; 
-	//printf("%f %f\n", theta_yaw, theta_angle);
+	theta_degree = round(angles::to_degrees(angles::normalize_angle_positive(theta_yaw)));
+}
+
+//function to decide the turn direction of the robot. 
+// true:-> turn clockwise, 
+// false :-> turn anticlockwise
+void decideTurnDirection(double goal_angle, double current_angle)
+{
+	if((goal_angle - current_angle ) > -180 && (goal_angle - current_angle ) <= 0)
+		clock_wise = true;
+	else
+		clock_wise = false;
+			
 }
 
 int main(int argc, char **argv)
 {
 	ros::init(argc, argv, "controlHusky");
-	ros::NodeHandle n;
+	ros::NodeHandle n("~");
+
+	n.getParam("x", goal_x);
+	n.getParam("y", goal_y);
 	
-	ros::Publisher pub = n.advertise<geometry_msgs::Twist>("/twist_marker_server/cmd_vel", 1000);
 	ros::Subscriber husky_sub = n.subscribe("/odometry/filtered", 1000, callBack);
+	ros::Publisher pub = n.advertise<geometry_msgs::Twist>("/twist_marker_server/cmd_vel", 1000);
 
 	ros::Rate loop_rate(10);
 	
-	double goal_angle;
-	//double goal_angle_1, goal_angle_2;
+	double goal_angle, goal, diff;
+	
 	while (ros::ok())
 	{
-		printf("Diff_x: %f 	Diff_y: %f\n", (goal_x - current_x), (goal_y - current_y));
+		geometry_msgs::Twist motion;
 
-		//while( (((goal_x - current_x) > 0.3) || ((goal_x - current_x) < -0.3)) && (((goal_y - current_y) >0.3) || ((goal_y - current_y) < -0.3)) )
-		//while((goal_x - current_x)>0 && (goal_y - current_y)>0)
-		if((goal_x - current_x)>0 && (goal_y - current_y)>0)
-		{
-			double goal = (atan(goal_y/goal_x));
-			goal_angle =  angles::to_degrees(angles::normalize_angle_positive(goal));
-			//printf("Goal Angle: %f Current Angle: %f \n", goal_angle, (theta_degree));
-		
-			geometry_msgs::Twist motion;
+		//printf("Diff_x: %f 	Diff_y: %f\n", (goal_x - current_x), (goal_y - current_y));
+		printf("Diff_x: %f 	Diff_y: %f\n", round(goal_x - current_x), round(goal_y - current_y));
+		printf("Current_x: %f	Current_y: %f\n", current_x, current_y);
 
-			double diff = abs(theta_degree - goal_angle);
-	//		printf("%f\n", diff);
-
-			if (diff != 0)
+		if ( (round(goal_x - current_x) == 0) && (round(goal_y - current_y) == 0) )
+		{//once reached the destination, align the robot in x direction
+			printf("STOPPING :::: Current Angle %f\n", theta_degree);
+			if(theta_degree != 0.00)
 			{
-	//		printf("%f \n", theta_degree);
+				if(theta_degree <= 180 && theta_degree > 0 )
+					motion.angular.z = -0.1;
+				else
+					motion.angular.z = 0.1;
+				pub.publish(motion);
+			}
+			else
+			{
+				ros::shutdown();
+			}
+
+		}
+		else 
+		{//reach the destination
+			if(((current_x)<= (goal_x)) && ((current_y) <= (goal_y))) //Case1
+			{
+				printf("1st Case\n");
+				goal = (atan((goal_y-current_y)/(goal_x-current_x)));
+				//goal_angle =  angles::to_degrees(angles::normalize_angle_positive(goal));
+				goal_angle =  abs(angles::to_degrees(angles::normalize_angle(goal)));
+				//clock_wise = false;				
+			}
+			else if(((current_x) >= (goal_x)) && ((current_y) <= (goal_y)))	//Case2
+			{
+				printf("2nd Case\n");
+				goal = (atan((goal_x-current_x)/(goal_y-current_y)));
+				//printf("goal angle positive %f\n", angles::to_degrees(angles::normalize_angle_positive(goal)));
+				//printf("goal angle normalized %f\n", angles::to_degrees(angles::normalize_angle(goal)));
+				goal_angle =  abs(angles::to_degrees(angles::normalize_angle(goal)))+90;
+				//clock_wise = false;									
+			}
+			if( ((current_x >= (goal_x)) && ((current_y) >= (goal_y)))) //|| ( ((round(current_x) >= (goal_x)) && ((current_y) >= (goal_y)))) )
+			{
+				printf("3rd Case\n");
+				goal = (atan((goal_y-current_y)/(goal_x-current_x)));
+				//goal_angle =  angles::to_degrees(angles::normalize_angle_positive(goal))+180;
+				//printf("goal angle normalized %f\n", angles::to_degrees(angles::normalize_angle(goal)));
+				goal_angle =  abs(angles::to_degrees(angles::normalize_angle(goal)));
+				goal_angle = goal_angle + 180.00; 
+				//clock_wise = false;							
+			}
+			else if((current_x < goal_x) && (current_y > goal_y))
+			{
+				printf("4th Case\n");
+				goal = (atan((goal_y-current_y)/(goal_x-current_x)));
+				//printf("goal without normalization %f\n", goal);
+				//goal_angle =  angles::to_degrees(angles::normalize_angle_positive(goal));	
+				goal_angle =  abs(angles::to_degrees(angles::normalize_angle(goal)));
+				goal_angle = 360- goal_angle;	
+				//clock_wise = true;		
+			}
+			
+			decideTurnDirection(goal_angle, theta_degree);
+
+			printf("Goal Angle: %f Current Angle: %f \n", goal_angle, (theta_degree));
+
+			diff = abs(round(goal_angle) - round(theta_degree));
+			
+			if (round(diff) != 0)
+			{
 			//align the robot
-		
-				motion.linear.x = 0.0;
-				motion.linear.y = 0.0;
-				motion.linear.z = 0.0;
-				motion.angular.x = 0.0;
-				motion.angular.y = 0.0;		
-				motion.angular.z = 0.1;
+				if(clock_wise == true)
+					motion.angular.z = -0.1;
+				else
+					motion.angular.z = 0.1;
 		
 				pub.publish(motion);
 			}
 			else
 			{//go straight
 				motion.linear.x = 0.2;
-				motion.linear.y = 0.0;
-				motion.linear.z = 0.0;
-				motion.angular.x = 0.0;
-				motion.angular.y = 0.0;		
-				motion.angular.z = 0.0;
 		
 				pub.publish(motion);		
 			}
 		}
+		
 		ros::spinOnce();
 		loop_rate.sleep();
 	}	
